be4d08fcde6dd94f865f62e5f991fbef84c7dfeb
bahouadonald <269331697+bahouadonald@users.noreply.github.com>
2026-08-23 15:01:34 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { CartProvider } from "./context/CartContext";
 import CataloguePlats from "./pages/CataloguePlats";
 import ProfilRestaurant from "./pages/ProfilRestaurant";
+import Panier from "./pages/Panier";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<CataloguePlats />} />
-        <Route path="/restaurant/:id" element={<ProfilRestaurant />} />
-      </Routes>
-    </BrowserRouter>
+    <CartProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<CataloguePlats />} />
+          <Route path="/restaurant/:id" element={<ProfilRestaurant />} />
+          <Route path="/panier" element={<Panier />} />
+        </Routes>
+      </BrowserRouter>
+    </CartProvider>
   );
 }
