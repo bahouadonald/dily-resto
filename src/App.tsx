1f4d00a2327630f470327c9de18fb6196d91afc7
bahouadonald <269331697+bahouadonald@users.noreply.github.com>
2026-08-23 10:30:22 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import CataloguePlats from "./pages/CataloguePlats";
+import ProfilRestaurant from "./pages/ProfilRestaurant";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<CataloguePlats />} />
-        {/* Prochaine étape : route /restaurant/:id pour le profil restaurant */}
+        <Route path="/restaurant/:id" element={<ProfilRestaurant />} />
       </Routes>
     </BrowserRouter>
   );
