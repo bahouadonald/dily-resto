bd527ab914194b419e14ff85ddeab25fa4259b95
bahouadonald <269331697+bahouadonald@users.noreply.github.com>
2026-08-23 15:17:14 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import CataloguePlats from "./pages/CataloguePlats";
 import ProfilRestaurant from "./pages/ProfilRestaurant";
 import Panier from "./pages/Panier";
 import Livraison from "./pages/Livraison";
+import Paiement from "./pages/Paiement";
+import SuiviCommande from "./pages/SuiviCommande";
 
 export default function App() {
   return (
@@ -14,6 +16,8 @@ export default function App() {
           <Route path="/restaurant/:id" element={<ProfilRestaurant />} />
           <Route path="/panier" element={<Panier />} />
           <Route path="/livraison" element={<Livraison />} />
+          <Route path="/paiement" element={<Paiement />} />
+          <Route path="/suivi/:id" element={<SuiviCommande />} />
         </Routes>
       </BrowserRouter>
     </CartProvider>
